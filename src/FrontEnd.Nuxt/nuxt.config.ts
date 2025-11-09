f858f3a145553530741fca7837150a5bd4f3c1c9
James Coliz <jcoliz@outlook.com>
2025-11-08 19:59:29 -0800

    build(nuxt): Add bootstrap

--- src/FrontEnd.Nuxt/nuxt.config.ts
+++ src/FrontEnd.Nuxt/nuxt.config.ts
@@ -4,5 +4,19 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   devServer: {
     port: parseInt(process.env.PORT ?? "5173"),
-  }  
+  },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          quietDeps: true,
+          // You can also silence specific deprecation types from your own code if needed
+          silenceDeprecations: ['import', 'color-functions'], 
+        },
+      },
+    },
+  },
+  css: [
+    '~/assets/scss/custom.scss'
+  ]
 })
