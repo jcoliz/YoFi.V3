802b5cae70ad2f05d8affce229574b492dd51e18
James Coliz <jcoliz@outlook.com>
2025-11-09 12:17:09 -0800

    feat(nuxt): Add @coliz/vue-base-controls
    And use feather icon 'box' for site header

--- src/FrontEnd.Nuxt/app/components/SiteHeader.vue
+++ src/FrontEnd.Nuxt/app/components/SiteHeader.vue
@@ -2,7 +2,7 @@
     <div class="container">
     <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
       <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-        <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+        <FeatherIcon class="me-2" icon="box" size="32"/>        
         <span class="fs-4">YoFi.V3</span>
       </a>
 
---
+++ src/FrontEnd.Nuxt/app/plugins/vue-base-controls.ts
@@ -0,0 +1,5 @@
+import controls from '@coliz/vue-base-controls'
+
+export default defineNuxtPlugin(nuxtApp => {
+    nuxtApp.vueApp.use(controls)
+})
