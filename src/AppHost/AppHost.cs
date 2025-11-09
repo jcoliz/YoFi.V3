d9b5e48b99007452ab38c4b3a45708ab82989a86
James Coliz <jcoliz@outlook.com>
2025-11-09 14:25:26 -0800

    refactor(blazor): Remove needless Blazor component
    Included as part of template boilerplate. Never worked anyway.

--- src/AppHost/AppHost.cs
+++ src/AppHost/AppHost.cs
@@ -3,14 +3,9 @@ var builder = DistributedApplication.CreateBuilder(args);
 var apiService = builder.AddProject<Projects.YoFi_V3_BackEnd>("backend")
     .WithHttpHealthCheck("/health");
 
-builder.AddProject<Projects.YoFi_V3_FrontEnd_Blazor>("frontend-blazor")
-    .WithExternalHttpEndpoints()
-    .WithHttpHealthCheck("/health")
-    .WithReference(apiService)
-    .WaitFor(apiService);
-
 builder.AddNpmApp("frontend-nuxt", "../FrontEnd.Nuxt")
     .WithReference(apiService)
+    .WaitFor(apiService)
     .WithHttpEndpoint(env: "PORT")
     .WithExternalHttpEndpoints()
     .PublishAsDockerFile();
--- src/FrontEnd.Blazor/README.md
+++
@@ -1,3 +0,0 @@
-# Blazor Front End
-
-This project was created originally as part of the template. It is not used and should be deleted.
