1ddf66104a130701db4af856999f74675d2e4b3e
James Coliz <jcoliz@outlook.com>
2025-11-11 09:34:00 -0800

    build(docker): Update start script to launch the browser [skip ci]

--- scripts/Start-Container.ps1
+++ scripts/Start-Container.ps1
@@ -1,1 +1,6 @@
+$ErrorActionPreference = "Stop"
 docker compose -f ./docker/docker-compose-ci.yml up -d --wait
+
+Write-Host "Containers are up and running."
+
+Start-Process "http://localhost:5000"
