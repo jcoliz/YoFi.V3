c4bfdfb7401315eb86938843236ca98545fed92a
James Coliz <jcoliz@outlook.com>
2025-11-10 20:50:08 -0800

    test(func): Run functional tests in container
    Complete!!

--- scripts/Run-FunctionalTestsVsContainer.ps1
+++ scripts/Run-FunctionalTestsVsContainer.ps1
@@ -8,5 +8,5 @@ Write-Output "Running tests..."
 dotnet test .\YoFi.V3.Tests.Functional.csproj -s .\docker.runsettings
 Pop-Location
 
-#Write-Output "Stopping docker services..."
-#Invoke-Expression "docker compose -f ./docker/docker-compose-ci.yml down"
+Write-Output "Stopping docker services..."
+Invoke-Expression "docker compose -f ./docker/docker-compose-ci.yml down"
