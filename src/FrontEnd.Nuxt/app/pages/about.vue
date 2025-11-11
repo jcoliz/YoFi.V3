913c7521708b1688f367dea3c11138dec62ccfa0
James Coliz <jcoliz@outlook.com>
2025-11-11 10:05:31 -0800

    build(nuxt): Prepare to use direct backend API calls, not proxied
    This begins to implement the recommendations in ADR #7. Just testing now to ensure it comes through during generation.
    
    Turns out the key is to ensure the env var is prefixed with "NUXT_PUBLIC_"

--- src/FrontEnd.Nuxt/app/pages/about.vue
+++ src/FrontEnd.Nuxt/app/pages/about.vue
@@ -1,8 +1,12 @@
 <template>
     <h1>About</h1>
+    <p>Testing the ability for <strong>static API base URLs</strong></p>
+    <p>Using backend API at {{ config.public.apiBaseUrl }}</p>
 </template>
 <script setup lang="ts">
 useHead({
   title: 'About',
 });
+const config = useRuntimeConfig()
+console.log('API Base URL:', config.public.apiBaseUrl)
 </script>
--- src/FrontEnd.Nuxt/nuxt.config.ts
+++ src/FrontEnd.Nuxt/nuxt.config.ts
@@ -11,7 +11,7 @@ export default defineNuxtConfig({
         scss: {
           quietDeps: true,
           // You can also silence specific deprecation types from your own code if needed
-          silenceDeprecations: ['import', 'color-functions'], 
+          silenceDeprecations: ['import', 'color-functions'],
         },
       },
     },
@@ -24,8 +24,18 @@ export default defineNuxtConfig({
       linkActiveClass: 'active'
     }
   },
-  routeRules: {//
-    //'/api/**': { cors: true, proxy: `${process.env.services__backend__http__0}/api/**` }
-    '/api/**': { proxy: `http://localhost:5379/api/**` }
+  routeRules: {
+    // This is used to proxy API requests during **development**
+    '/api/**': { cors: true, proxy: `${process.env.services__backend__http__0}/api/**` }
+  },
+  runtimeConfig: {
+    public: {
+      // For **CI** and **production**, the frontend is statically generated using
+      // `nuxt generate`. At that time, the NUXT_PUBLIC_API_BASE_URL environment variable
+      // is read from the build environment (e.g., Docker build ARG).
+      // NOTE: Nuxt generation reads NUXT_PUBLIC_* environment variables at build time
+      // ALSO NOTE: Using this `apiBaseUrl` is still a work in progress.
+      apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE_URL || 'http://localhost:5379'
+    }
   }
 })
