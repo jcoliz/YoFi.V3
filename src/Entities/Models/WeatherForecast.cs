a3956ac9aa5943cadcb99db84c631735fa1925ee
James Coliz <jcoliz@outlook.com>
2025-11-13 05:35:00 -0800

    test(int): Add Database Configuration and EF Core Behavior Tests

--- src/Entities/Models/WeatherForecast.cs
+++ src/Entities/Models/WeatherForecast.cs
@@ -10,5 +10,5 @@ public record WeatherForecast : IModel
     public int TemperatureC { get; set; }
     public string? Summary { get; set; }
 
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    public int TemperatureF => 32 + (int)(TemperatureC * 9 / 5);
 }
