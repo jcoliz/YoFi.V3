606a9f869c093a69ff14ddd682fd08e5c6f60237
James Coliz <James.Coliz@microsoft.com>
2025-11-14 12:19:25 -0800

    build: Add cover coverage
    Also fixes CI break.
    Also remove coverage from Integration tests, that's not a goal

---
+++ scripts/Collect-CodeCoverage.ps1
@@ -0,0 +1,15 @@
+param(
+    [Parameter()]
+    [string]
+    $Tests="Unit"
+)
+
+Push-Location "$PSScriptRoot/../tests/$Tests"
+$ErrorActionPreference = "Ignore"
+del TestResults -Recurse
+del bin\results -Recurse
+$ErrorActionPreference = "Stop"
+dotnet test --collect:"XPlat Code Coverage" --settings:coverlet.runsettings
+reportgenerator -reports:.\TestResults\*\coverage.cobertura.xml -targetdir:.\bin\result
+start .\bin\result\index.html
+Pop-Location
