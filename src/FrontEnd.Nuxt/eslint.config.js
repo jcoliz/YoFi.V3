f7121ad1d72e5ed5c97f07b97ac322b85dc4783c
James Coliz <jcoliz@outlook.com>
2025-11-19 17:22:32 -0800

    build(nuxt): Install prettier

--- src/FrontEnd.Nuxt/eslint.config.js
+++ src/FrontEnd.Nuxt/eslint.config.js
@@ -2,6 +2,8 @@
 import withNuxt from './.nuxt/eslint.config.mjs'
 import * as parserVue from 'vue-eslint-parser'
 import * as parserTypeScript from '@typescript-eslint/parser'
+import eslintPluginPrettier from 'eslint-plugin-prettier'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default withNuxt(
   // Override parser configuration for Vue files with TypeScript
@@ -15,11 +17,17 @@ export default withNuxt(
         sourceType: 'module',
       },
     },
+    plugins: {
+      prettier: eslintPluginPrettier,
+    },
     rules: {
+      ...eslintConfigPrettier.rules,
       // Relax some rules for better DX
       'vue/multi-word-component-names': 'off',
       // Vue 3 supports multiple root elements in templates
       'vue/no-multiple-template-root': 'off',
+      // Prettier integration
+      'prettier/prettier': 'warn',
     },
   },
 )
