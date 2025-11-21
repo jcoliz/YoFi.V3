a418ff31e70df2adf74b261197f268c2b854746b
James Coliz <jcoliz@outlook.com>
2025-11-20 18:51:25 -0800

    build(frontend): add @sidebase/nuxt-auth

--- src/FrontEnd.Nuxt/nuxt.config.ts
+++ src/FrontEnd.Nuxt/nuxt.config.ts
@@ -1,7 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  modules: ['@nuxt/eslint'],
+  modules: ['@nuxt/eslint', '@sidebase/nuxt-auth'],
+
   devtools: { enabled: true },
   eslint: {
     config: {
@@ -26,6 +27,56 @@ export default defineNuxtConfig({
   router: {
     options: {
       linkActiveClass: 'active',
+    },
+  },
+  auth: {
+    baseURL: `${process.env.services__backend__http__0}/api/auth`, // Update this to your backend URL
+    provider: {
+      type: 'local',
+      endpoints: {
+        signIn: { path: '/login', method: 'post' }, // ADD THIS LINE
+        signOut: { path: '/logout', method: 'post' },
+        getSession: { path: '/user' },
+        signUp: { path: '/signup', method: 'post' },
+      },
+      // not 'pages'??
+      pages: {
+        login: '/login', // Path to the login page (where unauthenticated users are sent)
+      },
+      token: {
+        signInResponseTokenPointer: '/token/accessToken',
+      },
+      refresh: {
+        isEnabled: true,
+        endpoint: { path: '/refresh', method: 'post' },
+        refreshOnlyToken: false,
+        token: {
+          signInResponseRefreshTokenPointer: '/token/refreshToken',
+          refreshResponseTokenPointer: '/token/accessToken',
+          refreshRequestTokenPointer: '/refreshToken',
+        },
+      },
+      session: {
+        dataType: {
+          id: 'string',
+          email: 'string',
+          name: 'string',
+          roles: 'string[]', // Updated to match your UserInfo model
+          claims: '{ type:string, value:string }[]',
+        },
+        dataResponsePointer: '/user',
+      },
+    },
+    sessionRefresh: {
+      // Whether to refresh the session every time the browser window is refocused.
+      enableOnWindowFocus: true,
+      // Whether to refresh the session every `X` milliseconds. Set this to `false` to turn it off. The session will only be refreshed if a session already exists.
+      enablePeriodically: 5000, // just for demo!!
+      // Custom refresh handler - uncomment to use
+      // handler: './config/AuthRefreshHandler'
+    },
+    globalAppMiddleware: {
+      isEnabled: true,
     },
   },
   runtimeConfig: {
