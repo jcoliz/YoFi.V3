eea74be933665dab2c5c981c8333dca425798767
James Coliz <jcoliz@outlook.com>
2025-12-08 10:35:51 -0800

    tests(integration): dotnet new nunit -o .\tests\Integration.Controller -n YoFi.V3.Tests.Integration.Controller

---
+++ tests/Integration.Controller/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace YoFi.V3.Tests.Integration.Controller;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
